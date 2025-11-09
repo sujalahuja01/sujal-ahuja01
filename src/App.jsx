a2cf8c927257b10d0121e5be4b4234b9d94e5236
sujalahuja01 <sujalahuja150@gmail.com>
2025-11-09 19:25:55 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="conatiner mx-auto max-7xl">
+      {/* navbar */}
+      {/* hero */}
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* testimonials */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  );
+};
+
+export default App;
