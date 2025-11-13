b1ef0214c7f53a7461ba1376a3386cdb29eb39ae
sujalahuja01 <sujalahuja150@gmail.com>
2025-11-13 19:13:08 +0530

    final done

--- src/constants/index.js
+++ src/constants/index.js
@@ -157,7 +157,7 @@ export const mySocials = [
   {
     id: 2,
     name: "Linkedin",
-    href: "www.linkedin.com/in/sujalahuja",
+    href: "https://www.linkedin.com/in/sujalahuja",
     icon: "/assets/socials/link.svg",
   },
   {
@@ -186,27 +186,6 @@ export const experiences = [
   //     "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
   //     "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
   //     "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
-  //   ],
-  // },
-  // {
-  //   title: "Back-End Developer",
-  //   job: "Car Manufacture",
-  //   date: "2023-2024",
-  //   contents: [
-  //     "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
-  //     "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
-  //     "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
-  //     "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
-  //     "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
-  //   ],
-  // },
-  // {
-  //   title: "Freelance Developer",
-  //   job: "Self-Employed",
-  //   date: "2025-Present",
-  //   contents: [
-  //     "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
-  //     "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
   //   ],
   // },
 ];
